6065a89bf657a76d2b8a73f4b8d070537ed470d0
wisesoft <atmtec@gmail.com>
2023-01-01 13:29:28 -0500

    added bootstrap support

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,18 +3,27 @@ import React from 'react'
 
 export default function Header() {
   return (
-   <div className='nav'>
-  
-    <div className='nav navbar navbar-expand-lg mx-4'><img src={<Logo/>} alt='logo'/> </div>
-    <div className='collapse navbar-lg'>
-        <ul className='nav navbar-nav text-capitalize mx-auto'>
-            <li className='navbar-item active'> </li>
-            <li className='navbar-item active'> </li>
-            <li className='navbar-item active'> </li>
-            <li className='navbar-item active'> </li>
-            <li className='navbar-item active'> </li>
-        </ul>
-    </div>
+     <div className="navbar navbar-expand-sm bg-primary navbar-dack px-sm-5">
+    
+    <div className="navbar-header">
+        <div className="container-fluid">
+
+          <div className="navbar-header">
+        <div className="navbar-brand">
+            COUNTDOWN
+            </div>
+            </div>
+            </div>
+            <ul className="nav navbar-nav">
+              <li className='nav-item active'>Details</li>
+              <li className='nav-item active'>Options</li>
+              <li className='nav-item active'>Settings</li>
+              <li className='nav-item active'>About</li>
+            </ul>
+
+        
+      </div>
+    
     </div>
   )
 }
